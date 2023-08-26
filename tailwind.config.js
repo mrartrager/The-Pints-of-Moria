7568615fa0bd523d8016ef6c981b9de2ab846a59
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-08-26 14:04:57 -0500

    added react and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
+
